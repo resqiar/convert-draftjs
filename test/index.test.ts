0c4fee538abad94ba48012ae31d88656b66f249c
resqiar <resqiageng@gmail.com>
2021-09-28 23:40:33 +0700

    test: test converter to support input both string or JSON format

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,17 @@
 import { convertDraftToArray } from '../src/index';
-import { mockData } from './mocks/data';
+import { rawData } from './mocks/data';
 
 describe('export convertDraft()', () => {
-  it('The function should return an array of characters', () => {
+  it('The function should return an array of characters (Parsed JSON)', () => {
     // expected result = [String, String, String,]
     const expectedResult = ['A', 'B', 'C'];
-    const parsedMockData = JSON.parse(mockData);
-    console.log(parsedMockData);
+    const parsedMockData = JSON.parse(rawData);
     expect(convertDraftToArray(parsedMockData)).toEqual(expectedResult);
+  });
+
+  it('The function should return an array of characters (Stringify JSON)', () => {
+    // expected result = [String, String, String,]
+    const expectedResult = ['A', 'B', 'C'];
+    expect(convertDraftToArray(rawData)).toEqual(expectedResult);
   });
 });
--- test/mocks/data.ts
+++ test/mocks/data.ts
@@ -1,2 +1,2 @@
-export const mockData =
+export const rawData =
   '{"blocks":[{"key":"5aeg1","text":"TEST","type":"header-one","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"ldr2","text":"A","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"3t8tg","text":"B","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"7ilfe","text":"C","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"4mmla","text":"alert(\\"Test!\\");","type":"code-block","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"8qm9p","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}';
