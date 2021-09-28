be17a5111dce1a160007ae6c9956ba6eb96390db
resqiar <resqiageng@gmail.com>
2021-09-28 23:39:49 +0700

    feat: automatically parse string draft result

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,13 @@ import { ParsedJson } from './types/parsedjson';
 import { Options } from './types/options';
 
 export function convertDraftToArray(
-  draftResult: ParsedJson,
+  draftResult: ParsedJson | string,
   options?: Options
 ) {
+  const parsedDraft: ParsedJson = parseDraftResult(draftResult);
   let parsedArray: string[] = [];
 
-  if (typeof draftResult === 'string') {
-    JSON.parse(draftResult);
-  }
-
-  draftResult.blocks.forEach(value => {
+  parsedDraft.blocks.forEach(value => {
     if (
       value.text.length > 0 &&
       value.type === (options?.select ?? 'unstyled')
@@ -21,4 +18,15 @@ export function convertDraftToArray(
   });
 
   return parsedArray;
+}
+
+/**
+ * If draftResult is in stringify mode,
+ * then parse to JSON. If not simply return back.
+ */
+export function parseDraftResult(draftResult: ParsedJson | string) {
+  if (typeof draftResult === 'string') {
+    return JSON.parse(draftResult);
+  }
+  return draftResult;
 }
